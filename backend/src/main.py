2ad8ec43609d949fe0e6473291c6abd917b90c51
Stella <stellagu2011@gmail.com>
2025-10-30 15:37:09 -0400

    feat: add api for definitions

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from src.routes import define_bp
 
-def temp():    
+def create_app():    
     app = Flask(__name__) 
     print("Test")
-    
+
+    app.register_blueprint(define_bp)
     return app
 
-app = temp()
+app = create_app()
