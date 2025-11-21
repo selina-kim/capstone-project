20ecea0d453b7a0f025fba84f0f8e17afb51b9d4
Stella-Gu <89406441+Stella-Gu@users.noreply.github.com>
2025-11-21 06:39:25 -0500

    feat: dictionary and translation api [CP-45] (#9)
    * Add Docker and Poetry for backend
    
    * docs: add README
    
    * feat: schema design [CP-55] (#2)
    
    * Add schema design for database
    
    * Add schema design for database
    
    * Add changes to user table
    
    * feat: add api for definitions
    
    * feat: setup backend auto update (#3)
    
    * chore: add requests to poetry and poetry version requirement
    
    * feat: allow auto app update on file change
    
    * feat: add translation api and example sentances from mw dict
    
    * test: add test cases for dictionary and translate api
    
    ---------
    
    Co-authored-by: marwa-khafagy <99355272+marwa-khafagy@users.noreply.github.com>
    Co-authored-by: Selina <selinakimdev@outlook.com>

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,11 +1,19 @@
 from flask import Flask
+from routes.dictionary import *
+from routes.translate import *
+from dotenv import load_dotenv
 
+load_dotenv()
 
-def temp():
-    app = Flask(__name__)
+def create_app():    
+    app = Flask(__name__) 
     print("Test")
+
+    app.config['JSON_AS_ASCII'] = False
+
+    app.register_blueprint(define_bp)
+    app.register_blueprint(translate_bp)
 
     return app
 
-
-app = temp()
+app = create_app()
