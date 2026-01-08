e1e73218fe433e7ca9e152d85b85a37fbb055dbc
Selina <selinakimdev@outlook.com>
2026-01-07 21:51:46 -0500

    chore: vscode configs

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,12 +1,20 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
-const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
+const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
 
 module.exports = defineConfig([
   expoConfig,
   eslintPluginPrettierRecommended,
   {
-    ignores: ['dist/*'],
+    ignores: ["dist/*"],
+  },
+  {
+    rules: {
+      "react/jsx-curly-brace-presence": [
+        "warn",
+        { props: "never", children: "never" },
+      ],
+    },
   },
 ]);
