7744512b54ead0500bbd131d147e9b5d9d58ed31
Selina <selinakimdev@outlook.com>
2026-03-13 02:49:52 -0400

    fix: make header visible in tabs layout

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -1,15 +1,47 @@
+import { AppLogo } from "@/assets/AppLogo";
 import { DecksIcon } from "@/assets/icons/DecksIcon";
 import { HelpIcon } from "@/assets/icons/HelpIcon";
 import { HomeIcon } from "@/assets/icons/HomeIcon";
 import { OpenBookIcon } from "@/assets/icons/OpenBookIcon";
+import { ProfileIcon } from "@/assets/icons/ProfileIcon";
 import { COLORS } from "@/constants/colors";
+import { SHADOWS } from "@/constants/shadows";
+import { useAuth } from "@/context/AuthContext";
 import { Tabs } from "expo-router";
+import { Pressable, View } from "react-native";
+
+function HeaderRight() {
+  const { signOut, user } = useAuth();
+
+  const handleProfilePress = () => {
+    signOut(); // TODO: temporary, replace with profile page later
+  }
+
+  if (!user) return null;
+
+  return (
+    <Pressable onPress={handleProfilePress} style={{ marginHorizontal: 15, width: 38 }}>
+      <ProfileIcon />
+    </Pressable>
+  );
+}
 
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        headerShown: false,
+        headerShown: true,
+        headerStyle: {
+          backgroundColor: COLORS.accent.primary,
+          ...SHADOWS.default,
+        },
+        headerTintColor: COLORS.text.primary,
+        headerTitle: () => (
+          <View style={{ width: 100 }}>
+            <AppLogo />
+          </View>
+        ),
+        headerRight: () => <HeaderRight />,
         tabBarActiveTintColor: COLORS.accent.secondary, // TODO?
         tabBarInactiveTintColor: COLORS.icon.outlinePrimary,
         tabBarShowLabel: false,
--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,25 +1,9 @@
-import { AppLogo } from "@/assets/AppLogo";
-import { ProfileIcon } from "@/assets/icons/ProfileIcon";
 import { COLORS } from "@/constants/colors";
-import { SHADOWS } from "@/constants/shadows";
+import { AuthProvider, useAuth } from "@/context/AuthContext";
 import { Arimo_400Regular, Arimo_700Bold } from "@expo-google-fonts/arimo";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
-import { ActivityIndicator, Pressable, View } from "react-native";
-import { AuthProvider, useAuth } from "@/context/AuthContext";
-
-function HeaderRight() {
-  const { signOut, user } = useAuth();
-
-  // Only show profile icon if user is authenticated
-  if (!user) return null;
-
-  return (
-    <Pressable onPress={signOut} style={{ marginHorizontal: 15, width: 38 }}>
-      <ProfileIcon />
-    </Pressable>
-  );
-}
+import { ActivityIndicator, View } from "react-native";
 
 function RootLayoutNav() {
   const { isLoading } = useAuth();
@@ -41,23 +25,9 @@ function RootLayoutNav() {
   }
 
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: COLORS.accent.primary,
-          ...SHADOWS.default,
-        },
-        headerTintColor: COLORS.text.primary,
-        headerTitle: () => (
-          <View style={{ width: 100 }}>
-            <AppLogo />
-          </View>
-        ),
-        headerRight: () => <HeaderRight />,
-      }}
-    >
+    <Stack>
       <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-      <Stack.Screen name="(tabs)" options={{ headerShown: true }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
   );
 }
